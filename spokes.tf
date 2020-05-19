48f8148d1a7f69a6cdd34cda55a8684c827816ce
sllynn <stuart@databricks.com>
2020-05-19 16:07:28 +0100

    added back extra spoke

--- spokes.tf
+++ spokes.tf
@@ -27,21 +27,21 @@ module "adb_spoke_stuart" {
   shared_datalake_name = local.shared_datalake_name
 }
 
-//module "adb_spoke_acme" {
-//  source = "./modules/spoke"
-//  workspace_vnet_cidr = "10.178.0.0/16"
-//  private_subnet_cidr =  "10.178.1.0/24"
-//  public_subnet_cidr = "10.178.2.0/24"
-//  privatelink_subnet_cidr = "10.178.3.0/24"
-//  spoke_name = "adb_spoke_acme"
-//  spoke_location = "UK South"
-//  spoke_resource_owner = "stuart@databricks.com"
-//  hub_rg_name = local.hub_rg_name
-//  hub_vnet_name = local.hub_vnet_name
-//  hub_vnet_id = local.hub_vnet_id
-//  azure_firewall_name = local.azure_firewall_name
-//  azure_firewall_privateip = local.azure_firewall_privateip
-//  azure_firewall_rules_priority = 210
-//  shared_datalake_id = local.shared_datalake_id
-//  shared_datalake_name = local.shared_datalake_name
-//}
+module "adb_spoke_acme" {
+  source = "./modules/spoke"
+  workspace_vnet_cidr = "10.178.0.0/16"
+  private_subnet_cidr =  "10.178.1.0/24"
+  public_subnet_cidr = "10.178.2.0/24"
+  privatelink_subnet_cidr = "10.178.3.0/24"
+  spoke_name = "adb_spoke_acme"
+  spoke_location = "UK South"
+  spoke_resource_owner = "stuart@databricks.com"
+  hub_rg_name = local.hub_rg_name
+  hub_vnet_name = local.hub_vnet_name
+  hub_vnet_id = local.hub_vnet_id
+  azure_firewall_name = local.azure_firewall_name
+  azure_firewall_privateip = local.azure_firewall_privateip
+  azure_firewall_rules_priority = 210
+  shared_datalake_id = local.shared_datalake_id
+  shared_datalake_name = local.shared_datalake_name
+}
